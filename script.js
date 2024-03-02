dd20c102cc31fc720a39f34b90c3fb823d03cd59
kairveeehh <jampu741@gmail.com>
2024-03-03 00:44:56 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(" .multiple-text" , {
+  strings:["Frontend Developer" , "Freelancer" ,
+  "Coder" , "UI/UX Developer" ,"Content writer"] ,
+  typeSpeed: 70 ,
+  backSpeed: 50 ,
+  backDelay: 1000 ,
+  loop: true})
+
+  
